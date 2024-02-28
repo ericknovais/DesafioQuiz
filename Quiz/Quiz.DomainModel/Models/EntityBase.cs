c77d84efbfd6c610bb809c28b03f9e94adfe50c9
ericknovais <erick.hora@outlook.com>
2024-02-28 15:18:10 -0300

    Criação da solução em C# do projeto Quiz

---
+++ Quiz/Quiz.DomainModel/Models/EntityBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz.DomainModel.Models
+{
+    public abstract class EntityBase
+    {
+        public int ID { get; set; }
+        public DateTime DataCadatro { get; set; }
+        public DateTime DataAtualizacao { get; set; }
+
+        public StringBuilder _msgErro = new StringBuilder()
+        public virtual void ValidarCampos()
+        {
+            if (_msgErro.Length > 0)
+                throw new Exception(_msgErro.ToString());
+        }
+    }
+}
