2540d47e3d1e08520ccb467e6a241fa74f5b7c14
ericknovais <erick.hora@outlook.com>
2024-02-28 15:22:16 -0300

    criação da branch Develop

--- Quiz/Quiz.DomainModel/Models/EntityBase.cs
+++ Quiz/Quiz.DomainModel/Models/EntityBase.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text;
 
 namespace Quiz.DomainModel.Models
 {
@@ -12,7 +8,7 @@ namespace Quiz.DomainModel.Models
         public DateTime DataCadatro { get; set; }
         public DateTime DataAtualizacao { get; set; }
 
-        public StringBuilder _msgErro = new StringBuilder()
+        public StringBuilder _msgErro = new StringBuilder();
         public virtual void ValidarCampos()
         {
             if (_msgErro.Length > 0)
