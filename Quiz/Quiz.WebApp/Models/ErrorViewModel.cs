098aa0b271d33c8735e64a7e1b360cc307e7edec
ericknovais <erick.hora@outlook.com>
2024-02-28 22:30:07 -0300

    Criação do projeto WebApp

---
+++ Quiz/Quiz.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Quiz.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
