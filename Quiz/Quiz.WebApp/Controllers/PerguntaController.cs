04525df60e856c0507d87407243d4266fce5d6fe
ericknovais <erick.hora@outlook.com>
2024-02-28 22:55:36 -0300

    ControllerPergunta

---
+++ Quiz/Quiz.WebApp/Controllers/PerguntaController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Quiz.DataAccess.Repositories;
+
+namespace Quiz.WebApp.Controllers
+{
+    public class PerguntaController : Controller
+    {
+        Repository repository = new Repository();
+        public IActionResult List()
+        {
+            try
+            {
+                return View(repository.Pergunta.ObterTodos());
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            
+        }
+    }
+}
